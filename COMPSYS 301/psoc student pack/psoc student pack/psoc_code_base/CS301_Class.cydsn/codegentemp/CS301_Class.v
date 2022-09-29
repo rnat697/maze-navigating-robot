a82f7dd2d95f2a33ede0f534d05f4a6039f0aac0
sm43576 <79734318+sm43576@users.noreply.github.com>
2022-09-29 17:22:05 +1300

    average light sensors attempt

--- COMPSYS 301/psoc student pack/psoc student pack/psoc_code_base/CS301_Class.cydsn/codegentemp/CS301_Class.v
+++ COMPSYS 301/psoc student pack/psoc student pack/psoc_code_base/CS301_Class.cydsn/codegentemp/CS301_Class.v
@@ -1,6 +1,6 @@
 // ======================================================================
 // CS301_Class.v generated from TopDesign.cysch
-// 09/21/2022 at 16:16
+// 09/29/2022 at 17:20
 // This file is auto generated. ANY EDITS YOU MAKE MAY BE LOST WHEN THIS FILE IS REGENERATED!!!
 // ======================================================================
 
