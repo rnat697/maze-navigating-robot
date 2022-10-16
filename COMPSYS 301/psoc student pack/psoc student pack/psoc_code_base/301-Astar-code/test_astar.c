7f849fd1f1f5fd319a4306aef099a7e20f9c50a4
rnat697 <rnat697@aucklanduni.ac.nz>
2022-10-17 11:29:57 +1300

    add

--- COMPSYS 301/psoc student pack/psoc student pack/psoc_code_base/301-Astar-code/test_astar.c
+++ COMPSYS 301/psoc student pack/psoc student pack/psoc_code_base/301-Astar-code/test_astar.c
@@ -81,10 +81,10 @@ void findDirections()
          } else {
                if (prevCol  == nextCol && prevRow == nextRow) {
                   // record U
-                  if(intersectionArray[currentRow][currentCol] == 1){ // check if its actually an intersection
-                     result[arrayPointer] = 3;
-                     arrayPointer++; 
-                  }
+                  // check if its actually an intersection
+                  result[arrayPointer] = 3;
+                  arrayPointer++; 
+                  
                }
                //record straight
                else if(intersectionArray[currentRow][currentCol] == 1){
@@ -110,10 +110,10 @@ void findDirections()
 
                if (prevCol  == nextCol && prevRow == nextRow) {
                   // uturn
-                  if(intersectionArray[currentRow][currentCol] == 1){ // check if its actually an intersection
-                     result[arrayPointer] = 3;
-                     arrayPointer++; 
-                  }
+                  // check if its actually an intersection
+                  result[arrayPointer] = 3;
+                  arrayPointer++; 
+                  
                }
                //record straight
                else if(intersectionArray[currentRow][currentCol] == 1){
@@ -139,10 +139,10 @@ void findDirections()
          } else {
                if (prevCol  == nextCol && prevRow == nextRow) { // U turn
                   // record uturn
-                  if(intersectionArray[currentRow][currentCol] == 1){ // check if its actually an intersection
-                     result[arrayPointer] = 3;
-                     arrayPointer++; 
-                  }
+                  // check if its actually an intersection
+                  result[arrayPointer] = 3;
+                  arrayPointer++; 
+                  
                }
                //record straight
                else if(intersectionArray[currentRow][currentCol] == 1){
@@ -166,10 +166,10 @@ void findDirections()
                
                if (prevCol  == nextCol && prevRow == nextRow) {
                   // record u turn
-                  if(intersectionArray[currentRow][currentCol] == 1){ // check if its actually an intersection
-                     result[arrayPointer] = 3;
-                     arrayPointer++; 
-                  }
+                  // check if its actually an intersection
+                  result[arrayPointer] = 3;
+                  arrayPointer++; 
+                  
                }
                else if(intersectionArray[currentRow][currentCol] == 1){
                    //record straight
